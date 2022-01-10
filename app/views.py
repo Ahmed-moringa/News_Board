76e1f0d7e45b024023dae859dc1ad0726668b2ea
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2022-01-11 02:05:15 +0300

    added functionality to navigate to address

--- app/views.py
+++ app/views.py
@@ -10,10 +10,10 @@ def index():
     '''
     return render_template('index.html')
 
-@app.route('/article/<article_id>')
+@app.route('/article/<int:article_id>')
 def article(article_id):
 
     '''
     View article page function that returns the article details page and its data
     '''
-    return render_template('movie.html',id = article_id)
+    return render_template('article.html',id = article_id)
