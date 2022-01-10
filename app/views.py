967ca0780c056060341c9645d0a997fcb4414e64
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2022-01-11 02:01:28 +0300

    added article route

--- app/views.py
+++ app/views.py
@@ -9,3 +9,11 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+@app.route('/article/<article_id>')
+def article(article_id):
+
+    '''
+    View article page function that returns the article details page and its data
+    '''
+    return render_template('movie.html',id = article_id)
